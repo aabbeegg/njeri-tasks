d89f296b1627b112b8756d985b314e5eac99fc6f
bev @nun <86957585+aabbeegg@users.noreply.github.com>
2023-02-01 10:39:30 +0200

    task1

---
+++ exe1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<body>
+
+            <h1>PHP is interesting</h1>
+
+<?php include"l.header.php"?>
+<?php
+//opening range
+echo "Hello World<br>";
+echo "This is \"very \" good<br>";
+echo "Njeri, BBCAP23";
+echo "sup <br>";
+
+?>
+<h2>Current date</h2>
+<?php echo "My name is \"Njeri\"<br>";
+echo date("d.m.Y")?>
+
+<?php include"l.footer.php" ?>
+</body>    
+</head>
+</html>
